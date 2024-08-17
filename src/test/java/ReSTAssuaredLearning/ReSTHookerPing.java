ccce5b075284438face169116ebad8ce770f716c
Siddharth Richhariya <siddharth.richhariya123@gmail.com>
2024-08-17 22:49:08 +0530

    RestAssured Library

--- src/test/java/ReSTAssuaredLearning/ReSTHookerPing.java
+++ src/test/java/ReSTAssuaredLearning/ReSTHookerPing.java
@@ -1,2 +1,17 @@
-package ReSTAssuaredLearning;public class ReSTHookerPing {
+package ReSTAssuaredLearning;
+
+import io.restassured.RestAssured;
+
+public class ReSTHookerPing {
+    public static void main(String[] args) {
+
+        RestAssured
+                .given()
+                .baseUri("https://restful-booker.herokuapp.com")
+                .basePath("/ping")
+                .when()
+                .get()
+                .then()
+                .statusCode(202);
+    }
 }
