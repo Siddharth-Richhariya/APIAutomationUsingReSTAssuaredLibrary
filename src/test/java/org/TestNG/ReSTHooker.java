f4068ad226621c84bd4409b31312063559112fab
Siddharth Richhariya <siddharth.richhariya123@gmail.com>
2024-08-17 23:12:28 +0530

    TestNG

--- src/test/java/org/TestNG/ReSTHooker.java
+++ src/test/java/org/TestNG/ReSTHooker.java
@@ -6,7 +6,16 @@ import org.testng.annotations.Test;
 public class ReSTHooker {
 
     @Test
-    public void getRequest(){
+    public void getRequest1(){
+        RestAssured
+                .given()
+                .baseUri("https://restful-booker.herokuapp.com")
+                .basePath("/ping")
+                .when()
+                .get().then().statusCode(201);
+    }
+    @Test
+    public void getRequest2(){
         RestAssured
                 .given()
                 .baseUri("https://restful-booker.herokuapp.com")
